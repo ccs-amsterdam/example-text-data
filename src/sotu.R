703dacc113e3934e46943e5fb4b0d060dd737977
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-01-13 12:53:49 +0100

    ADd Year to sotu

--- src/sotu.R
+++ src/sotu.R
@@ -1,13 +1,17 @@
+# Read and parse state of the union data sets from wolfram
 
 library(tidyverse)
-x = read_csv("~/Downloads/State-of-the-Union-Addresses.csv") %>% select(President:Text)
+url = "https://www.wolframcloud.com/objects/ec61dd71-4b02-458c-a30c-3536376dc3b9"
+x = read_csv(url) %>% select(President, Year, Date, Party, Text) 
 x$President = stringi::stri_match_first(x$President, regex=' "(.*)::')[,2]
 x$President = gsub("([A-Z])([A-Z])", "\\1. \\2", x$President)
 x$President = gsub("([A-Z])([A-Z])", "\\1. \\2", x$President)
 x$President = gsub("([a-z])([A-Z])", "\\1 \\2", x$President)
 x$President  = gsub("Mc Kinley", "McKinley", x$President)
-x
+
 
 x$Date = stringi::stri_match_first(x$Date, regex='\\{(.*)\\}')[,2]
+x$Date = as.Date(x$Date, "%Y, %m, %d")
 
-x$President = gsub("([A-Z])([A-Z])", "\\1. \\2", x$Date)
+x$Party = gsub("Missing[\"NotApplicable\"]", 'N/A', x$Party, fixed=T)
+write_csv(x, "~/example-text-data/sotu.csv")
